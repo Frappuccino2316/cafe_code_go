f06c95d8ca6d8b7bd9bf568522a02983c85e4ca7
Frappuccino001 <fumi00hiro@icloud.com>
2021-12-31 10:05:03 +0900

    ランダムに時間がかかるようにする

--- installs/installs.go
+++ installs/installs.go
@@ -1,14 +1,25 @@
 package installs
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func PrintInstall() {
 	maxCount := 50
 	currentCount := 0
 
 	for currentCount < maxCount {
+		sleepRate := time.Duration(rand.Intn(50))
+		if sleepRate == 0 {
+			continue
+		}
+
 		fmt.Print("#")
 		currentCount++
+
+		time.Sleep(time.Second / sleepRate)
 	}
 	fmt.Println(" Done!!")
 }
