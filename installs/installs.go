97a98f38cb1c58148b4e2bd6f464592a09b4ff85
Frappuccino001 <fumi00hiro@icloud.com>
2022-01-02 09:42:27 +0900

    繰り返しにした

--- installs/installs.go
+++ installs/installs.go
@@ -10,16 +10,18 @@ func PrintInstall() {
 	maxCount := 50
 	currentCount := 0
 
-	for currentCount < maxCount {
-		sleepRate := time.Duration(rand.Intn(50))
-		if sleepRate == 0 {
-			continue
+	for {
+		for currentCount < maxCount {
+			sleepRate := time.Duration(rand.Intn(50))
+			if sleepRate == 0 {
+				continue
+			}
+
+			fmt.Print("#")
+			currentCount++
+
+			time.Sleep(time.Second / sleepRate)
 		}
-
-		fmt.Print("#")
-		currentCount++
-
-		time.Sleep(time.Second / sleepRate)
+		fmt.Println(" Done!!")
 	}
-	fmt.Println(" Done!!")
 }
