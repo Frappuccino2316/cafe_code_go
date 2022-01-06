21ebec4d840e154066f91c1c3c392c3695e6ab59
Frappuccino001 <fumi00hiro@icloud.com>
2022-01-06 23:37:47 +0900

    コメントを追加

--- installs/installs.go
+++ installs/installs.go
@@ -7,39 +7,63 @@ import (
 )
 
 func PrintInstall() {
-	maxCount := 50
-	currentCount := 0
-	packageList := [...]string{"hoge", "fuga", "coffee-code", "go-package", "sample", "json-parset", "yokozuna", "kakamimochi", "ramen", "mac"}
-	alertMsg := `Warning: This package is old version!!
-	You should install newer version!!
-	`
+	// 時刻からランダムなシード値を設定
+	rand.Seed(time.Now().UnixNano())
 
+	// #を50文字出力する
+	maxCount := 50
+	// #をカウントする
+	currentCount := 0
+	// 出力用のダミーパッケージ名
+	packageList := [...]string{
+		"hoge",
+		"fuga",
+		"coffee-code",
+		"go-package",
+		"sample",
+		"json-parset",
+		"yokozuna",
+		"kakamimochi",
+		"ramen",
+		"mac",
+	}
+	// ダミーのアラートメッセージ
+	alertMsg := `Warning: This package is old version!!
+	You should install newer version!!`
+
+	// パッケージのダウンロードを繰り返す
 	for {
+		// 0~9でランダムに生成
 		randomNum := rand.Intn(10)
 
+		// パッケージ名をランダムに出力
 		fmt.Printf("go: downloading %s", packageList[randomNum])
 		fmt.Println("")
 
+		// 出力している#が50未満なら#を出力する
 		for currentCount < maxCount {
+			// ランダムに時間をかけているように見せるための数値を生成
 			sleepRate := time.Duration(rand.Intn(50))
-
-			if sleepRate == 0 {
-				continue
-			}
 
 			fmt.Print("#")
 			currentCount++
+
+			// sleepRateが0ならsleepしない
+			if sleepRate == 0 {
+				continue
+			}
 			time.Sleep(time.Second / sleepRate)
 		}
 
 		fmt.Println(" Done!!")
 
-		if randomNum == 0 {
+		// randomNumが0ならダミーのアラートを出す
+		if randomNum == 0 || randomNum == 1 || randomNum == 2 {
 			fmt.Printf("%s", alertMsg)
+			fmt.Println("")
 		}
 
-		if currentCount == 50 {
-			currentCount = 0
-		}
+		// 最後に#のカウントを0に戻す
+		currentCount = 0
 	}
 }
